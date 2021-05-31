b82b5cfb6190399c6e8c1e50eeb51e994a8dfc12
chromers <digitalartcreative@hotmail.com>
2021-05-31 19:32:04 +0700

    next js doc template themes free download gratis

---
+++ README.md
@@ -0,0 +1,59 @@
+# Next JS documentation website themes template
+
+free download source code gratis
+
+[test drive demo →](https://beposresto.vercel.app/)
+
+[read documentation →](https://www.hockeycomputindo.com/2021/05/create-website-using-next-js-free.html)
+
+For build modern website documentation using nestra next js framework
+
+
+------------
+
+with light and dark mode
+
+![free download next js website template themes gratis](https://1.bp.blogspot.com/-942LuVNEA6k/YLTP9MEZfsI/AAAAAAAAOU8/zEB6vLqXU-UzR3lHocOcSPiNMlVuJNklwCLcBGAsYHQ/s1348/free%2Bwebapp%2Bthemes%2Btemplate%2Bnext%2Bjs%2B%25281%2529.png)
+
+Home page with light mode
+
+![free download next js website template themes gratis](https://1.bp.blogspot.com/-pjDg1mS-cJo/YLTP9KRKtSI/AAAAAAAAOVA/BKsVEYR_-BoiE1e8UPkzDTBDIssYHzEjQCLcBGAsYHQ/s1348/free%2Bwebapp%2Bthemes%2Btemplate%2Bnext%2Bjs%2B%25282%2529.png)
+
+Home page article on dark mode
+
+![free download next js website template themes gratis](https://1.bp.blogspot.com/-Q_mhc4ziLXE/YLTP9nmwV3I/AAAAAAAAOVE/4hNlVNCmABk6dy-yzUxIBhyCBpKjnxP9wCLcBGAsYHQ/s1349/free%2Bwebapp%2Bthemes%2Btemplate%2Bnext%2Bjs%2B%25284%2529.png)
+
+documentation article non image light mode
+
+![free download next js website template themes gratis](https://1.bp.blogspot.com/-j3xA0R5ABLI/YLTP8qM2j_I/AAAAAAAAOU4/az0k8hQGzDY_rrZdG7b99VZjNwdLNypwgCLcBGAsYHQ/s1349/free%2Bwebapp%2Bthemes%2Btemplate%2Bnext%2Bjs%2B%25283%2529.png)
+
+documentation non image dark mode
+
+![free download next js website template themes gratis](https://1.bp.blogspot.com/-XEl0OtC8HGc/YLTP-cGdeuI/AAAAAAAAOVM/caMTZ5biTH8iuc-LHH3iwR_sTMCVa07iwCLcBGAsYHQ/s1349/free%2Bwebapp%2Bthemes%2Btemplate%2Bnext%2Bjs%2B%25286%2529.png)
+
+documentation article with image light mode
+
+![free download next js website template themes gratis](https://1.bp.blogspot.com/-_QhJjrcIOSs/YLTP-ChPFLI/AAAAAAAAOVI/4DdBWpLHXtc7rYVue8pIO3h4ELvcuzazACLcBGAsYHQ/s1349/free%2Bwebapp%2Bthemes%2Btemplate%2Bnext%2Bjs%2B%25285%2529.png)
+
+documentation article with image on dark mode
+
+
+------------
+
+How to install ??
+
++ Need node js
++ Download Source code
++ Extract file download on project folder
+
+Now run command using node
++ npm install
++ npm start
+
+open web browser localhost:3000
+
+---------------
+
+Setting website and change article doc, just [read documentation →](https://www.hockeycomputindo.com/2021/05/create-website-using-next-js-free.html)
+
+
---
+++ theme.config.js
@@ -0,0 +1,65 @@
+export default {
+  repository: 'https://mesinkasir.github.io',
+  titleSuffix: ' Aplikasi restoran Bepos Resto',
+  logo: (
+    <>
+      <span className="mr-2 font-extrabold hidden md:inline">Bepos</span>
+      <span className="text-gray-600 font-normal hidden md:inline">
+        Aplikasi Restoran
+      </span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="Content-Language" content="id" />
+      <meta name="description" content="Aplikasi restoran,software program restoran murah" />
+      <meta name="og:description" content="Aplikasi restoran,software program restoran murah" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content="https://1.bp.blogspot.com/-fSrj4SMwZFQ/YLOoUBVmA2I/AAAAAAAAOSs/xBXQpVaz4ukSXz_geEjv0hyjxNwL32gTACLcBGAsYHQ/s1366/program%2Brestoransoftware%2Bkasir%2Bmurah%2B3.png" />
+      <meta name="twitter:site:domain" content="berposresto.vercel.app" />
+      <meta name="twitter:url" content="https://beposresto.vercel.app" />
+      <meta name="og:title" content="APlikasi restoran berpos resto" />
+      <meta name="og:image" content="https://1.bp.blogspot.com/-fSrj4SMwZFQ/YLOoUBVmA2I/AAAAAAAAOSs/xBXQpVaz4ukSXz_geEjv0hyjxNwL32gTACLcBGAsYHQ/s1366/program%2Brestoransoftware%2Bkasir%2Bmurah%2B3.png" />
+      <meta name="apple-mobile-web-app-title" content="APlikasi restoran berpos resto" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="https://github.com/fluidicon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="https://github.com/fluidicon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="https://github.com/fluidicon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="https://github.com/fluidicon.png"
+      />
+      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+    </>
+  ),
+  search: true,
+  prevLinks: true,
+  nextLinks: true,
+  footer: true,
+  footerEditOnGitHubLink: false,
+  footerText: <>MIT {new Date().getFullYear()} © Bepos Resto.</>,
+}
